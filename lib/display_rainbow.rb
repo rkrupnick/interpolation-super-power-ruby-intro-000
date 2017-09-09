5e54027be1a829ee6cfbcfc14a51ce2f12e31f54
Rachel Krupnick <rachel@rachelkrupnick.com>
2017-09-09 01:00:57 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -2,6 +2,11 @@
 def display_rainbow(colors)
   letters = ["R", "O", "Y", "G", "B", "I", "V"]
   puts letters[0] + ":" + colors[0]
+  puts letters[1] + ":" + colors[1]
+  puts letters[2] + ":" + colors[2]
+  puts letters[3] + ":" + colors[3]
+  puts letters[4] + ":" + colors[4]
+  puts letters[5] + ":" + colors[5]
+  puts letters[6] + ":" + colors[6]
 end
 
-display_rainbow(["red", "orange"])
